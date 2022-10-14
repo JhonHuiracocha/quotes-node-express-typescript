12f28aa024cabf2aaef9c6a35ed3b425afa59ae1
Jhon Huiracocha <jhonhuiracocha123456789@gmail.com>
2022-10-14 00:42:00 -0500

    user routes updated: get and delete user by id added

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -4,6 +4,8 @@ import { userController } from "../controllers";
 const userRoutes = Router();
 
 userRoutes.post("/", userController.createUser);
+userRoutes.get("/:uid", userController.getUserById);
 userRoutes.get("/:uid/quotes", userController.getUserQuotes);
+userRoutes.delete("/:uid", userController.deleteUserById);
 
 export { userRoutes };
