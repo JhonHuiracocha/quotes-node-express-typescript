969ac5ffd01c881971229a59e0e57a27c4f5a468
Jhon Huiracocha <jhonhuiracocha123456789@gmail.com>
2022-10-17 22:58:16 -0500

    auth validator updated: register added

--- src/validators/auth.validator.ts
+++ src/validators/auth.validator.ts
@@ -16,3 +16,25 @@ export const validateUserLogin = [
     .withMessage("The password must have a maximum of 255 characters"),
   validateRequestSchema,
 ];
+
+export const validateUserRegister = [
+  check("username")
+    .not()
+    .isEmpty()
+    .withMessage("The username cannot be empty")
+    .isLength({ max: 45 })
+    .withMessage("The username must have a maximum of 45 characters"),
+  check("email")
+    .not()
+    .isEmpty()
+    .withMessage("The email cannot be empty")
+    .isEmail()
+    .withMessage("Must be a email"),
+  check("password")
+    .not()
+    .isEmpty()
+    .withMessage("The password cannot be empty")
+    .isLength({ max: 255 })
+    .withMessage("The password must have a maximum of 255 characters"),
+  validateRequestSchema,
+];
